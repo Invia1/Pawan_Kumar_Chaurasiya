11b583daa80b0cf3e4d11d7d53efa2085e342b73
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-17 17:01:07 +0530

    Add files via upload

---
+++ RIM_Hospital Test1/RIM_Hospital/InterfacePatient/InterfacePatient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using RIM_Hospital.Model;
+using RIM_Hospital.Funtionality;
+
+
+namespace RIM_Hospital.InterfacePatient
+{
+    interface InterfacePat
+    {
+       
+        List<PatientDec> GetPatient();
+        List<PatientDec> PatientRegistration(PatientDec plr);
+    }
+}
