c0c1102b3ed5fe53bd83af03f4b6a6fa7814b50d
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-22 14:54:28 +0530

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary.User_UI
+{
+    class Main_UI
+    {
+        Dictionary<int, string> DicList = new Dictionary<int, string>();
+
+
+
+        public void Addfunc()
+        {
+            DicList.Add(1,"Pawan");
+            DicList.Add(2, "Pawan");
+            DicList.Add(3, "Pawan");
+            DicList.Add(4, "Pawan");
+            DicList.Add(5, "Pawan");
+
+        }
+        public Dictionary<int,string> Show_Record()
+        {
+            return DicList;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Dictionery Program");
+
+            Main_UI er = new Main_UI();
+            er.Addfunc();
+            Console.WriteLine("Total number of Record Found");
+            er.Show_Record();
+            foreach (var item in er.DicList)
+            {
+                Console.WriteLine("Emp Id is :- "+item.Key+", EmpName is :- "+item.Value);
+            }
+            
+            
+
+            Console.ReadLine();
+            
+
+
+
+        }
+
+
+    }
+}
