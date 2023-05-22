53962b795c2d68e6d77ca5fc34e4547e81c0a3fc
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-22 15:13:10 +0530

    Add files via upload

---
+++ GetSetProperties.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GetSet
+{
+    class Pvt
+    {
+        int id;
+        string name;
+        public string pname
+        {
+            get
+            {
+                return name; 
+            }
+            set
+            {
+                name = value;  
+            }
+        }
+        public int pid
+        {
+            get
+            {
+                return id; 
+            
+            }
+            set 
+            {
+                id = value;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pvt obj = new Pvt();
+            obj.pid = 23;
+            Console.WriteLine(obj.pid);
+
+            Pvt obj1 = new Pvt();
+            obj1.pname = "subodh";
+            Console.WriteLine(obj1.pname);
+            Console.ReadLine();            
+
+        }
+    }
+}
